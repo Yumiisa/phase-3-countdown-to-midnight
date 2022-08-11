0077545641b97bcf0b5fb7ad19a852202e594164
Gaudencia <conny@students.uonbi.ac.ke>
2022-08-11 20:17:09 +0300

    commit tree 162cc3e0fd770e7eabb7d588cb0851ebd2e7ee2d

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
-#write your code here
+#write your code here   
 
-def countdown
+def countdown number
+    
+    x =1
+while x <=number
+    
+    puts "#{number} SECOND(S)!"
+   
+    number -=1
+     
+     
+end
+
+
+'HAPPY NEW YEAR!' 
+end 
+def countdown_with_sleep seconds
+date1 = Time.now + seconds
+  while Time.now < date1
+    t = Time.at(date1.to_i - Time.now.to_i)
+    p t.strftime('%H:%M:%S')
+    sleep 1
+  end
 end
